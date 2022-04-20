e27b6000de1689c60088238236fcf38533b69551
janghyoseog <bestdevhyo1225@gmail.com>
2022-04-20 22:40:27 +0900

    Feat: 예외 포함과 스택 트레이스 예제 추가

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -16,6 +16,16 @@ public class UncheckedAppTest {
         assertThatThrownBy(controller::request).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
+    }
+
     static class Controller {
         Service service = new Service();
 
